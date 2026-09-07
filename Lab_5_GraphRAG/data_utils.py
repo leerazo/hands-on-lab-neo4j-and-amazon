d847b5d7faabb348407e75aa7817e24cd5309760
leerazo <flrazo@gmail.com>
2026-09-07 07:53:48 +0000

    fixed bugs

--- Lab_5_GraphRAG/data_utils.py
+++ Lab_5_GraphRAG/data_utils.py
@@ -65,7 +65,7 @@ def get_llm() -> BedrockLLM:
     config = BedrockConfig()
 
     return BedrockLLM(
-        model_id=config.model_id,
+        #model_id=config.model_id,
         region_name=config.region,
     )
 
